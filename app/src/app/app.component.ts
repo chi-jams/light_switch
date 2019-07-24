a050ce654ea79e25abfdf5027b076dff0578239b
Daichi Jameson <djameson@mines.edu>
2019-07-23 22:12:24 -0600

    Got a color picker onto the Angular webpage

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ColorPickerService } from 'ngx-color-picker';
 
 @Component({
   selector: 'app-root',
+  moduleId: 'src/app/app.component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private cpService: ColorPickerService) { }
+
+  public onChange(color: string): string {
+    const hsva = this.cpService.stringToHsva(color, true);
+    console.log(hsva);
+    return '';
+  }
 }
